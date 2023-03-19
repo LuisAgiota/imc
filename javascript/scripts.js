a9671ce37263ca9ef6cf0b9353e16c90292cccbb
LuisHenrique <luishenriquecorporativo@gamil.com>
2023-03-19 01:06:05 -0300

    IMC Completo

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,1 +1,22 @@
-console.log("teste");
+function calculate(){
+var height = document.getElementById("height").value
+var weight = document.getElementById("weight").value
+
+var imc = weight / height ** 2;
+if (imc < 18.5) {
+    window.alert("Você está abaixo do peso");
+
+}else if (imc < 24.9) {
+    window.alert("Você está normal");
+
+}else if (imc < 29.9) {
+    window.alert ("Você está com sobrepeso");
+
+}else if (imc < 39.9) {
+    window.alert ("Você está com obesidade");
+
+}else if (imc > 39.9) {
+    window.alert ("Você está com obesidade morbida");
+
+}
+}
